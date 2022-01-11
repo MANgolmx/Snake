c695c6c1044bd71491b69bc0915f8c55b0dece1e
MANgolmx <misammmmm@inbox.ru>
2022-01-11 17:21:35 +0300

    пока

--- Snake/Source.cpp
+++ Snake/Source.cpp
@@ -11,9 +11,7 @@ int random(int a, int b)
 	return rand() % (b - a + 1) + a;
 }
 
-//хай
-
-bool isBelong(int x, int y, SDL_Rect r)
+bol isBelong(int x, int y, SDL_Rect r)
 {
 	if (x >= r.x && x <= r.x + r.w &&
 		y >= r.y && y <= r.y + r.h)
