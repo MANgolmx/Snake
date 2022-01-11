72fe5360830582ca9aff0266c510215af3325e17
MANgolmx <misammmmm@inbox.ru>
2022-01-11 17:27:55 +0300

    Исправлена ошибка.

--- Snake/Source.cpp
+++ Snake/Source.cpp
@@ -11,7 +11,7 @@ int random(int a, int b)
 	return rand() % (b - a + 1) + a;
 }
 
-bol isBelong(int x, int y, SDL_Rect r)
+bool isBelong(int x, int y, SDL_Rect r)
 {
 	if (x >= r.x && x <= r.x + r.w &&
 		y >= r.y && y <= r.y + r.h)
